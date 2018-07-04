1aec3bc682c18a9c9b473386b68f6faaae432a04
Alexey Stepanov <penguinolog@gmail.com>
2018-07-04 13:02:30 +0200

    Bump to version 1.0.5
    * Build 3.7 wheels
    * slim sdist
    * main tests on 3.7
    
    Signed-off-by: Alexey Stepanov <penguinolog@gmail.com>

--- threaded/__init__.py
+++ threaded/__init__.py
@@ -66,7 +66,7 @@ if GThreadPooled is not None:  # pragma: no cover
         'gthreadpooled'
     )
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __author__ = "Alexey Stepanov"
 __author_email__ = 'penguinolog@gmail.com'
 __maintainers__ = {
