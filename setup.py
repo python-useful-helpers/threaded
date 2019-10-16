4bbe38ac1a9c5301911e5967dd0b853a069517d6
Aleksei Stepanov <aleksei.stepanov@brightcomputing.com>
2019-10-16 18:04:08 +0200

    Do not cythonize on windows: not supported

--- setup.py
+++ setup.py
@@ -52,18 +52,15 @@ with open("README.rst") as f:
 
 
 # noinspection PyCallingNonCallable
-if cythonize is not None:
-    if "win32" != sys.platform:
-        REQUIRES_OPTIMIZATION = [
-            setuptools.Extension("threaded.class_decorator", ["threaded/class_decorator.pyx"]),
-            setuptools.Extension("threaded._base_threaded", ["threaded/_base_threaded.py"]),
-            setuptools.Extension("threaded._asynciotask", ["threaded/_asynciotask.pyx"]),
-            setuptools.Extension("threaded._threaded", ["threaded/_threaded.pyx"]),
-            setuptools.Extension("threaded._threadpooled", ["threaded/_threadpooled.py"]),
-            setuptools.Extension("threaded.__init__", ["threaded/__init__.pyx"]),
-        ]
-    else:
-        REQUIRES_OPTIMIZATION = []
+if cythonize is not None and "win32" != sys.platform:
+    REQUIRES_OPTIMIZATION = [
+        setuptools.Extension("threaded.class_decorator", ["threaded/class_decorator.pyx"]),
+        setuptools.Extension("threaded._base_threaded", ["threaded/_base_threaded.py"]),
+        setuptools.Extension("threaded._asynciotask", ["threaded/_asynciotask.pyx"]),
+        setuptools.Extension("threaded._threaded", ["threaded/_threaded.pyx"]),
+        setuptools.Extension("threaded._threadpooled", ["threaded/_threadpooled.py"]),
+        setuptools.Extension("threaded.__init__", ["threaded/__init__.pyx"]),
+    ]
     INTERFACES = ["class_decorator.pxd", "_asynciotask.pxd", "_threaded.pxd"]
 
     EXT_MODULES = cythonize(
